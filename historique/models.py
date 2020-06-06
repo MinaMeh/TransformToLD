0a4a3ebbaf4980a9690f7051c6e078ae08777a2e
MekImEne <fi_mekideche@esi.dz>
2020-06-06 06:12:06 +0200

    Add serializers

--- historique/models.py
+++ historique/models.py
@@ -5,7 +5,7 @@ class inputOpenData(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     file = models.FileField()
     link = models.CharField(max_length=200, blank=False, default='')
-    description = models.CharField(max_length=500, blank=False, default='')
+    description = models.TextField()
     addition_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -14,7 +14,7 @@ class inputOpenData(models.Model):
 
 class Project(models.Model):
     project_name = models.CharField(max_length=70, blank=False, default='')
-    description = models.CharField(max_length=400, blank=False, default='')
+    description = models.TextField()
     licence = models.CharField(max_length=100, blank=False, default='')
     author = models.CharField(max_length=50, blank=False, default='')
     updated = models.BooleanField(default=False)
