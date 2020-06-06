826cfa68f69a2fbbadb76cff32c6078d07106a6a
MekImEne <fi_mekideche@esi.dz>
2020-06-06 06:25:17 +0200

    edit Serializers

--- historique/serializers.py
+++ historique/serializers.py
@@ -1,18 +1,11 @@
 from rest_framework import serializers
 from historique.models import Project, inputOpenData
 
-
 class inputOpenDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = inputOpenData
         fields = '__all__'
-
-    def create(self, validated_data):
-        """
-        Create and return a new `input Open Data` instance, given the validated data.
-        """
-        return inputOpenData.objects.create(**validated_data)
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -22,23 +15,3 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
-
-    def create(self, validated_data):
-
-        inputOpenDataFile_data = validated_data.pop('inputOpenDataFile')
-        inputOpenDataFile = inputOpenData.objects.create(
-            **inputOpenDataFile_data)
-
-        project = Project.objects.create(
-            inputOpenDataFile=inputOpenDataFile, **validated_data)
-        return project
-
-    def update(self, instance, validated_data):
-        project = Project.objects.update(**validated_data)
-        return project
-
-
-class ProjectUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ['updated']
--- historique/views.py
+++ historique/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+from historique.models import Project, inputOpenData
+from historique.serializers import ProjectSerializer, inputOpenDataSerializer
