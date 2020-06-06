a28708817ab01cadb0ab0e6654ffd12a733ad762
MekImEne <fi_mekideche@esi.dz>
2020-06-06 07:31:14 +0200

    edit db

--- historique/models.py
+++ historique/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class inputOpenData(models.Model):
-    name = models.CharField(max_length=50, blank=False, default='')
-    file = models.FileField()
-    link = models.CharField(max_length=200, blank=False, default='')
-    description = models.TextField()
-    addition_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "%s" % (self.name)
-
-
 class Project(models.Model):
     project_name = models.CharField(max_length=70, blank=False, default='')
     description = models.TextField()
@@ -19,7 +8,19 @@ class Project(models.Model):
     author = models.CharField(max_length=50, blank=False, default='')
     updated = models.BooleanField(default=False)
     creation_date = models.DateTimeField(auto_now_add=True)
-    inputFile = models.ForeignKey(inputOpenData, on_delete=models.CASCADE ,null=True)
 
     def __str__(self):
         return "%s" % (self.project_name)
+
+
+class inputOpenData(models.Model):
+    name = models.CharField(max_length=50, blank=False, default='')
+    file = models.FileField()
+    link = models.CharField(max_length=200, blank=False, default='')
+    description = models.TextField()
+    addition_date = models.DateTimeField(auto_now_add=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return "%s" % (self.name)
+
