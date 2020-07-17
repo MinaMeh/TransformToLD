aa463bdb3bce6eee76822ad05f84a8f2d775c935
amina <fa_meherhera@esi.dz>
2020-07-17 13:30:50 +0100

    edit allowed-hosts

--- linkeddata/settings.py
+++ linkeddata/settings.py
@@ -161,4 +161,3 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-ALLOWED_HOSTS = ['linkeddata-app.herokuapp.com']
